8e38db87595e7a4c2aacb3e5951078149a377588
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-02-21 17:05:44 +0400

    task completed add_number

--- tasks/easy/functions/add_number.py
+++ tasks/easy/functions/add_number.py
@@ -13,3 +13,14 @@ add_two(3)  # 5
 add_three = add_numb(3)
 add_three(3) # 6
 """
+
+
+def add_numb(num: int):
+    def inner(new_num: int):
+        return num + new_num
+    return inner
+
+
+result = add_numb(4)
+
+
