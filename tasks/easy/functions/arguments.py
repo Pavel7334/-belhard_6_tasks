c52ecf5fe18711d9301edd3408d10d647b8de8ec
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-02-27 15:14:19 +0400

    task completed arguments

--- tasks/easy/functions/arguments.py
+++ tasks/easy/functions/arguments.py
@@ -18,3 +18,20 @@ https://pyneng.readthedocs.io/ru/latest/book/10_useful_functions/all_any.html
     "kwargs_max_len": 7
 }
 """
+
+
+def dict_from_args(*args, **kwargs):
+
+    if not all(type(arg) == int or type(arg) == float for arg in args):
+        raise TypeError("Все позиционные аргументы должны быть целыми")
+    args_sum = sum(args)
+
+    if not all(type(kwarg) == str and type(value) == str for kwarg, value in kwargs.items()):
+        raise TypeError("Все аргументы - ключевые слова должны быть строками")
+    new_list = list(kwargs.keys()) + list(kwargs.values())
+    kwargs_max_len = len(max(new_list, key=len))
+
+    return {
+        "args_sum": args_sum,
+        "kwargs_max_len": kwargs_max_len,
+    }
