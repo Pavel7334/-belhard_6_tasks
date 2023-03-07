a06d4bd5b966773fc764d66be37ddd5e165ec784
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-03-07 12:07:37 +0400

    task completed fibonacci_rec

--- tasks/easy/reqursion/fibonacci.py
+++ tasks/easy/reqursion/fibonacci.py
@@ -1,3 +1,12 @@
 """
 Написать рекурсивную функцию fibonacci, которая будет возвращать n-ый элемент
 """
+
+
+def fibonacci(num):
+    if num <= 1:
+        return num
+    return fibonacci(num - 1) + fibonacci(num - 2)
+
+
+fibonacci(1)
