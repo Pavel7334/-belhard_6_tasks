2cc50e3aba1398253dbdf2716ecc2fcb4d76aed2
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-03-08 01:00:00 +0400

    task completed chat_counter

--- tasks/medium/char_counter.py
+++ tasks/medium/char_counter.py
@@ -14,3 +14,17 @@
 Нельзя пользоваться collections.Counter!
 """
 STR_VAL = 'python is the fastest-growing major programming language'
+
+
+def count_char(str_val):
+    char_counter = {}
+    for char in str_val:
+        if char not in char_counter.keys():
+            char_counter[char] = 1
+        else:
+            char_counter[char] += 1
+
+    return char_counter
+
+
+count_char(STR_VAL)
