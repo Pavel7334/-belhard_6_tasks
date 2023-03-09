d58dd86c821ec3ea09796765a2c84f44b72fcd43
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-03-09 21:50:54 +0400

    task completed check_number

--- tasks/medium/check_number.py
+++ tasks/medium/check_number.py
@@ -5,3 +5,31 @@ False, если нет
 
 Нельзя пользоваться операцией возведения в степень
 """
+
+
+def check_number(n):
+    if n == 1:
+        return True
+    if n % 2 != 0:
+        return False
+    return check_number(n // 2)
+
+
+check_number(10)
+print(check_number(10))
+
+
+
+
+
+
+
+    # return True if (n & (n - 1)) == 0 else False      # 1-e решение
+
+
+
+    # if num == 1:                                      2-e решение
+    #     return True
+    # if num & 1:
+    #     return False
+    # return check_number(num >> 1)
