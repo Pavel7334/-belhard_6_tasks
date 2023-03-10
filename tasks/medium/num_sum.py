8ccce84e314fb9bf6393f1b138df5bab1852ba81
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-03-10 15:55:21 +0400

    task completed num_sum

--- tasks/medium/num_sum.py
+++ tasks/medium/num_sum.py
@@ -4,3 +4,22 @@
 
 Можно пользоваться только функциями, операторами и условиями.
 """
+
+
+def sum_of_numbers(n):
+    if n < 10:
+        return n
+    return n % 10 + sum_of_numbers(n // 10)
+
+
+foo = sum_of_numbers(23)
+print(foo)
+
+
+# def sum_of_numbers(num):
+#     sum_integers = 0
+#     for num in nums:
+#         if isinstance(num, int):
+#             sum_integers += num
+#         return sum_of_numbers(num + num)
+
